908948bb1a482a8253bb8dc70e74745ad327e88a
yuta.iwasaki <yuta825217@gmail.com>
2019-05-22 08:53:42 +0000

    CRUD tasks2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,7 +13,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task=Task.new(tasks_params)
+    @task=Task.new(task_params)
     
     if @task.save
        flash[:success] = 'Task が正常に投稿されました'
@@ -49,3 +49,10 @@ class TasksController < ApplicationController
   end
   
 end
+
+private
+
+  # Strong Parameter
+  def task_params
+    params.require(:task).permit(:content)
+  end
